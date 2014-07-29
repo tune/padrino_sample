989228178550e2506ee8104cd20c4acccc057835
Yuichi TSUNEMATSU <tsunematsu.yuuichi@canon.co.jp>
2014-07-29 14:05:35 +0900

    add cache function to simple controller
    http://localhost:3000/?lang=ja
      -> 'こんにちは Padrino'
    
    logger output
      DEBUG -  SET CACHE (0.0000s) /?lang=ja
      DEBUG -        GET (0.0155s) /?lang=ja - 200 OK
    
    http://localhost:3000/?lang=en
      -> 'こんにちは Padrino'
      DEBUG -  GET CACHE (0.0005s) /?lang=ja
      DEBUG -        GET (0.0055s) /?lang=en - 200 OK

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,7 @@
 Sample::App.controllers do
-  get :index do
+  get :index, cache: true do
+    cache_key request.path_info + "?" + params.slice('lang').to_param
+
     if params[:lang] == 'ja'
       'こんにちは Padrino'
     else
